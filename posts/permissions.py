646747307b3d925191284813bcd611b52696ef43
osm3rr <osm3rr@adakademy.com>
2025-02-10 11:03:42 -0400

    custom permissions done

---
+++ posts/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        # Authenticated users only can see list of posts
+        if request.user.is_authenticated:
+            return True
+        return False
+    
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Write permissions are only allowed to the author of the post
+        return obj.author == request.user
